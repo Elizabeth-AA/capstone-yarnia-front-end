fc22db48201401e51737c61cdb972b1702bc43e9
ElizabethAwesome <elizabeth2awesome@gmail.com>
2023-05-08 20:32:53 -0700

    creating axios req to get search from Ravelry

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,9 @@
-import axios from '@services/axios'
+import { instance, instanceRav } from '@services/axios'
 import routes from '@services/routes.json'
 
 export async function getRavelryYarn() {
     try {
-        const response = await axios.get(routes.stash)
+        const response = await instanceRav.get(routes.search)
         if (response.status === 200) {
             return response.data
         }
