405b22cfd3a1adcdd71c9269048b62fe7e0d2e82
Elizabeth-AA <elizabeth2awesome@gmail.com>
2023-05-10 13:00:49 -0700

    display modal with selected item data on click, and close dropdown

--- src/pages/Stash.jsx
+++ src/pages/Stash.jsx
@@ -10,8 +10,8 @@ import { getRavelryYarn } from "@utils/helpers"
 export default function Stash() {
     const [searchTerm, setSearchTerm] = useState("")
     const [items, setItems] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [selectedItem, setSelectedItem] = useState()
+    // const [loading, setLoading] = useState(false)
+    const [selectedItem, setSelectedItem] = useState(null)
 
     const search = async (searchTerm) => {
         try {
@@ -42,16 +42,42 @@ export default function Stash() {
     
     const handleSelectItem = (item) => {
         setSelectedItem(item);
+        document.activeElement.blur();
         console.log(item)
     }
 
     return (
-        <Autocomplete
-            items={items}
-            search={search}
-            value={searchTerm}
-            onChange={(item) => setSearchTerm(item)}
-            onSelect={handleSelectItem}
-        />
+        <>
+            <Autocomplete
+                items={items}
+                search={search}
+                value={searchTerm}
+                onChange={(item) => setSearchTerm(item)}
+                onSelect={handleSelectItem}
+            />
+        
+            {selectedItem && (
+                <div className="modal-open={opened}">
+                    <div className="modal-box">
+                        <img src={selectedItem.photo.medium_url} />
+                        <h3>{selectedItem.name}</h3>
+                        <p>
+                            {selectedItem.yarn_company} - {selectedItem.yarn_weight.name} - 
+                            {selectedItem.yardage} yds
+                        </p>
+                        <p>{selectedItem.description}</p>
+                        <button>close</button>
+                        <button>add to stash</button>
+                        <a
+                            href={`https://www.ravelry.com/yarns/library/${selectedItem.permalink}`}
+                            target="_blank"
+                            className="btn btn-primary"
+                        >
+                            View on Ravelry
+                        </a>
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
