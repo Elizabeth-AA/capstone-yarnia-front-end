a0d022845b0edee9cc0fd2081b7ef1c7702c29ef
Elizabeth-AA <elizabeth2awesome@gmail.com>
2023-05-12 15:45:04 -0700

    feature: signup page

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -1,3 +1,16 @@
+import AuthHeader from "@components/Auth/AuthHeader"
+import RegisterForm from "@components/Auth/RegisterForm"
+
 export default function SignUp() {
-    
+    return (
+        <>
+            <AuthHeader
+                heading="Signup to find inspiration"
+                text="Already have an account? "
+                linkText="Login"
+                linkUrl="/login"
+            />
+            <RegisterForm />
+        </>
+    )
 }
