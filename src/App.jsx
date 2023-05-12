cf2cb8711f91e585fe21ee1cf476f20ee7674172
Elizabeth-AA <elizabeth2awesome@gmail.com>
2023-05-12 10:08:47 -0700

    set up pages and route structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Header from '@components/Header/Header'
+import { LogIn, SignUp } from '@pages/Auth'
 import Stash from '@pages/Stash'
+import Projects from '@pages/Projects'
 
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path='/stash' element={<Stash />} />
-      </Routes>
+      <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 md:px-8">
+        <div className="max-w-md w-full space-y-8">
+        <Routes>
+          <Route path='/' element={<LogIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/stash' element={<Stash />} />
+          <Route path='/projects' element={<Projects />} />
+        </Routes>
+        </div>
+      </div>
     </>
   )
 }
