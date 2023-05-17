76695a5584c659b86e30834b072ff008e32851ae
Elizabeth-AA <elizabeth2awesome@gmail.com>
2023-05-16 21:46:04 -0700

    draft about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Header from '@components/Header/Header'
+import About from '@pages/About'
 import User from '@pages/User'
 import Projects from '@pages/Projects'
 import Landing from '@pages/Landing'
@@ -10,8 +11,8 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/stash' element={<User />} />
+        <Route path='/' element={<About />} />
+        <Route path='/users' element={<User />} />
         <Route path='/yarn/:rav_id' element={<Projects />} />
       </Routes>
     </>
