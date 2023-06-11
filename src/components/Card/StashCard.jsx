ad32502ff4bcf701c87b733e42d9fd97a759397a
Elizabeth-AA <elizabeth2awesome@gmail.com>
2023-06-11 13:43:07 -0700

    fix: responsive style for stash card

--- src/components/Card/StashCard.jsx
+++ src/components/Card/StashCard.jsx
@@ -30,7 +30,7 @@ export default function StashCard({ item }) {
         alt="skein of yarn"
         className="h-40 w-44 rounded-t-lg object-cover md:h-64 md:w-48 md:rounded-l-lg md:rounded-tr-none"
       />
-      <div className="relative w-full px-4 py-3">
+      <div className="relative w-44 md:w-full px-4 py-3">
         <span tabIndex={0} className="card__subheader">
           {item.yarn_company}
         </span>
@@ -44,8 +44,8 @@ export default function StashCard({ item }) {
           {renderListItem('Machine Washable:', item.machine_washable)}
           {renderListItem('Texture:', item.texture)}
         </ul>
-        <div className="btn-tip" data-tip="see projects">
-          <button onClick={() => toProjects(item)} className="absolute left-0 top-0 icon">
+        <div className="absolute bottom-0 right-0 btn-tip" data-tip="see projects">
+          <button onClick={() => toProjects(item)} className="icon">
             <img src={projects_direct} />
           </button>
         </div>
