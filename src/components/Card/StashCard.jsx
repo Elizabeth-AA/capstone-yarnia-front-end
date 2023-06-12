9c91dabfa298c6c848fc04342e60668adffec317
Elizabeth-AA <elizabeth2awesome@gmail.com>
2023-06-11 21:57:38 -0700

    refactor: using card contents component for selected item modal

--- src/components/Card/StashCard.jsx
+++ src/components/Card/StashCard.jsx
@@ -9,47 +9,13 @@ export default function StashCard({ item }) {
     navigate(`/yarn/${item.rav_id}`, { state: { item } })
   }
 
-  const renderListItem = (label, value) => {
-    if (value) {
-      let renderedValue = value;
-      if (label === 'Machine Washable:' && value === true) {
-        renderedValue = 'Machine Washable';
-      }
-      return (
-        <li tabIndex={0} className="card__details">
-          {renderedValue}
-        </li>
-      );
-    }
-    return null;
-  }
-
   return (
     <div tabIndex={0} className="card" key={item.rav_id}>
-      <img
-        src={item.photo.small_url}
-        alt="skein of yarn"
-        className="h-40 w-44 rounded-t-lg object-cover md:h-64 md:w-48 md:rounded-l-lg md:rounded-tr-none"
-      />
-      <div className="relative w-44 md:w-full px-4 py-3">
-        <span tabIndex={0} className="card__subheader">
-          {item.yarn_company}
-        </span>
-        <p tabIndex={0} className="card__header">
-          {item.name}
-        </p>
-        <ul className="flex list-disc flex-wrap items-center">
-          {renderListItem('Yarn Weight:', item.yarn_weight.name)}
-          {renderListItem('Yardage:', `${item.yardage} yds`)}
-          {renderListItem('Grams:', `${item.grams} grams`)}
-          {renderListItem('Machine Washable:', item.machine_washable)}
-          {renderListItem('Texture:', item.texture)}
-        </ul>
-        <div className="absolute bottom-0 right-0 btn-tip" data-tip="see projects">
-          <button onClick={() => toProjects(item)} className="icon">
-            <img src={projects_direct} />
-          </button>
-        </div>
+      <YarnCardContents item={item} />
+      <div className="absolute bottom-0 right-0 btn-tip" data-tip="see projects">
+        <button onClick={() => toProjects(item)} className="icon">
+          <img src={projects_direct} />
+        </button>
       </div>
     </div>
   )
