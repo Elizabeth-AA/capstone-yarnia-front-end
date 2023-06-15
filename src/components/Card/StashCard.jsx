69c13b565aa4d67313e21ff653618802cc1f8298
Elizabeth-AA <elizabeth2awesome@gmail.com>
2023-06-14 19:01:39 -0700

    refactor: changed icon for stash card

--- src/components/Card/StashCard.jsx
+++ src/components/Card/StashCard.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom'
+import { LightBulbIcon } from '@heroicons/react/24/outline'
 import YarnCardContents from '@components/Card/YarnCardContents'
-import projects_direct from '@assets/icons/yarnia-projects.png'
 
 export default function StashCard({ item }) {
   const navigate = useNavigate()
@@ -13,8 +13,8 @@ export default function StashCard({ item }) {
     <div tabIndex={0} className="card" key={item.rav_id}>
       <YarnCardContents item={item} />
       <div className="absolute bottom-0 right-0 btn-tip" data-tip="see projects">
-        <button onClick={() => toProjects(item)} className="icon">
-          <img src={projects_direct} />
+        <button onClick={() => toProjects(item)}>
+          <LightBulbIcon className='icon' />
         </button>
       </div>
     </div>
