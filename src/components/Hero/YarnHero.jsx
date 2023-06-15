190b2cc9737a3f01ea03b1fee4dc590330e37628
Elizabeth-AA <elizabeth2awesome@gmail.com>
2023-06-14 21:17:07 -0700

    refactor: made the project page hero item image the background

--- src/components/Hero/YarnHero.jsx
+++ src/components/Hero/YarnHero.jsx
@@ -1,12 +1,16 @@
+import HeroCard from "../Card/HeroCard"
+
 const YarnHero = ({ item }) => {
     return (
-        <div className="hero bg-secondary flex flex-col md:flex-row py-4 justify-evenly">
-            <figure className="justify-start max-h-56 md:max-h-64"><img className="opacity-80 rounded" src={item.photo.small_url} alt="skein of yarn" /></figure>
-            <div className="hero-content max-h-56 md:max-h-64 bg-accent-content rounded text-primary gap-2 flex flex-wrap md:flex-col">
-                <h1 className="card-title">{item.name}</h1>
-                <p>by {item.yarn_company}</p>
-                <p>{item.yarn_weight.name}</p>
-                <p>{item.yardage} yds</p>
+        <div
+            className="hero bg-secondary flex flex-col py-4 justify-evenly"
+            style={{backgroundImage: `url(${item.photo.small_url})`}}
+        >
+            <div className="hero-overlay bg-opacity-80"></div>
+            <div className="hero-content max-h-56 md:max-h-64 lg:max-h-72 bg-accent-content bg-opacity-75 rounded text-primary gap-2 flex flex-wrap flex-col">
+                <HeroCard
+                item={item}
+                />
             </div>
         </div>
     )
