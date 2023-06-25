15150ec2fd942b596e03f5260a3dfb11c99ece48
Elizabeth-AA <elizabeth2awesome@gmail.com>
2023-06-24 22:21:47 -0700

    fix: re-rendering stash card section

---
+++ src/components/Cards/StashCardSection.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import StashCard from "@components/Cards/StashCard"
+
+export default function StashCardSection({ stash }) {
+
+    const [stashItems, setStashItems] = useState([])
+    const sortedStash = stash.sort((a, b) => b.yarn_id - a.yarn_id)
+
+    useEffect(() => {
+        setStashItems(sortedStash)
+    }, [stash])
+
+  return (
+    <div className="section-border">
+        <div className="section-content">
+            <section className="w-full mx-auto flex flex-wrap justify-center">
+                {stashItems.map((item) => (
+                    <StashCard
+                        key={item.rav_id}
+                        item={item}
+                    />
+                ))}
+            </section>
+        </div>
+    </div>
+  )
+}
